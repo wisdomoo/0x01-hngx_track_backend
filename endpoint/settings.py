d45d6e4edc6463cb0b25ecc8a033c6d7cf206b0f
wisdom <okoroaforwisdomo@gmail.com>
2023-09-09 07:02:52 +0100

    files uploaded

--- endpoint/settings.py
+++ endpoint/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=(emfbc_xh=s8$-ypdxm^9dca0ytezmk69sp16zp^_ea%1cemt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['endpoints-uw4l.onrender.com']
 
 
 # Application definition
