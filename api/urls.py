359652330bb71f4c4c86a5f01afb8e2d2aedee88
wisdom <okoroaforwisdomo@gmail.com>
2023-09-09 07:00:31 +0100

    files uploaded

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from api.views import get_info
+from api.views import get_info1,get_info
 
 urlpatterns = [
+    path('', get_info1, name='get_info1'),
     path('', get_info, name='get_info'),
+    
 ]
--- api/views.py
+++ api/views.py
@@ -4,6 +4,23 @@ from django.http import JsonResponse
 from django.utils import timezone
 from api.models import Info
 
+
+
+def get_info1(request):
+   
+    response_data = {
+        "slack_name": 'slack_name',
+        "current_day": 'current_day',
+        "utc_time": "timezone.now().strftime('%Y-%m-%dT%H:%M:%SZ')",
+        "track": 'track',
+        "github_file_url": "https://github.com/wisdomoo/hngx-track_backend/blob/master/endpoint/api/views.py",
+        "github_repo_url": "https://github.com/wisdomoo/hngx-track_backend",
+        "status_code": 200
+    }
+
+    return JsonResponse(response_data)
+    
+
 def get_info(request):
     slack_name = request.GET.get('slack_name')
     track = request.GET.get('track')
@@ -14,14 +31,17 @@ def get_info(request):
     info.save()
 
     response_data = {
-        "slack_name": 'slack_name',
+        "slack_name": slack_name,
         "current_day": current_day,
         "utc_time": timezone.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
-        "track": 'track',
+        "track": track,
         "github_file_url": "https://github.com/wisdomoo/hngx-track_backend/blob/master/endpoint/api/views.py",
         "github_repo_url": "https://github.com/wisdomoo/hngx-track_backend",
         "status_code": 200
     }
 
     return JsonResponse(response_data)
+    
+
+
 
